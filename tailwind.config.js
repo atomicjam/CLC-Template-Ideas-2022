8c8b7ff23d333faed7a266db4924618453a3dbda
Elliot Goode <atomicjam@gmail.com>
2022-12-02 16:01:46 +0000

    updated lots

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     extend: {
             colors: {
                 'clc-green': 'rgb(117,142,136)', // old colour #677b6e rgb(116,128,128)
-                'clc-pea': 'rgb(212,212,210)',
+                'clc-pea': 'rgb(215,217,216)',
                 'clc-gray': 'rgb(50, 50, 50)',
                 'clc-silver': '#c0c0c0',
                 'clc-navy': 'rgba(48, 55, 98, 1)',
@@ -40,12 +40,17 @@ module.exports = {
                 '50%': { transform: 'translateY(-50px)' },
                 '100%': { transform: 'translateY(0px)' },
               },
+              wiggle: {
+                '0%, 100%': { transform: 'rotate(-3deg)' },
+                '50%': { transform: 'rotate(3deg)' },
+              },
             },
             animation: {
               'slide-image-a': 'slideA 15s linear infinite',
               'slide-image-b': 'slideD 15s linear infinite',
               'slide-text-a': 'slideB 15s linear infinite',
               'slide-text-b': 'slideC 15s linear infinite',
+              'wiggle-once': ' wiggle 0.5s ease-in-out 5s 6',
             },
     },
   },
